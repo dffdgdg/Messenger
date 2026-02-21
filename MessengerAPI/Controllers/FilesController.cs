6310dc45695cd7fd72c9a424280870200dfc9ebe
Шихов Александр <86885674+dffdgdg@users.noreply.github.com>
2026-02-21 20:24:45 +0300

    Исправление ошибок

--- MessengerAPI/Controllers/FilesController.cs
+++ MessengerAPI/Controllers/FilesController.cs
@@ -10,8 +10,9 @@ namespace MessengerAPI.Controllers;
 public class FilesController(IFileService fileService, IChatService chatService, ILogger<FilesController> logger)
     : BaseController<FilesController>(logger)
 {
+    private const long MaxFileSizeBytes = 100 * 1024 * 1024;
     [HttpPost("upload")]
-    [RequestSizeLimit(100 * 1024 * 1024)]
+    [RequestSizeLimit(MaxFileSizeBytes)]
     public async Task<ActionResult<ApiResponse<MessageFileDTO>>> Upload(
         [FromQuery] int chatId, IFormFile file)
     {
